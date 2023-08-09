66c252fde7bc08b3b1e5c2bf9f4562a0ea13966e
ayush <ayushgevariya8914@gmail.com>
2023-08-09 04:38:07 -0800

    just small bug fixed

--- routes/items.js
+++ routes/items.js
@@ -6,24 +6,24 @@ const router = express.Router();
 const { getAllItems, getAllItemsTesting, addItem, addAllItems, updateItem, deleteItem,deleteAllItems} = require('../controllers/items');
 
 //get items
-router.route('/').get( getAllItems  );
+// router.route('/').get( getAllItems  );
 
 //delete item
-router.route('/:id').delete( deleteItem );
+// router.route('/:id').delete( deleteItem );
 
 //delete all
-router.route('/').delete( deleteAllItems );
+// router.route('/').delete( deleteAllItems );
 
 //add item
-router.route('/').post( addItem );
+// router.route('/').post( addItem );
 
 //add all items
-router.route('/').post( addAllItems );
+// router.route('/').post( addAllItems );
 
 //update item
-router.route('/:id').patch( updateItem );
+// router.route('/:id').patch( updateItem );
 
 //testing
-router.route('/testing').get( getAllItemsTesting);
+// router.route('/testing').get( getAllItemsTesting);
 
 module.exports = router;    
