55b959d18d23521595084a59db45b7b9a48496fe
ayush <ayushgevariya8914@gmail.com>
2023-08-11 20:54:27 -0800

    cors removed

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 require('dotenv').config();
 const experss = require('express');
 const BodyParser = require('body-parser');
-const cors = require('cors');
+
 const items_routes = require('./routes/items'); 
 const restaurants_routes = require('./routes/restaurant');
 const user_routes = require('./routes/userRoutes');
 const connectDB = require('./db/connect');
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
 const app = experss();
 const PORT = process.env.PORT || 5000;
