0b261e84ca0af71b2779f6772cc8964a8d5a2834
ayush <ayushgevariya8914@gmail.com>
2023-08-11 20:51:45 -0800

    cors configured

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const restaurants_routes = require('./routes/restaurant');
 const user_routes = require('./routes/userRoutes');
 const connectDB = require('./db/connect');
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const app = experss();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions ={
-    origin:'localhost:3000', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
 app.use(BodyParser.json());
 app.use('/api/items', items_routes  );
